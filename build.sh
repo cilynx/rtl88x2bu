f05722311f3354d1ceca64a80c80dfc725ab3522
Richie Zhang <12566991+StaticallyTypedRice@users.noreply.github.com>
2020-04-26 19:28:28 -0700

    Create build script.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cd rtl88x2bu
+VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
+sudo dkms add -m rtl88x2bu -v ${VER}
+sudo dkms build -m rtl88x2bu -v ${VER}
+sudo dkms install -m rtl88x2bu -v ${VER}
+sudo modprobe 88x2bu
