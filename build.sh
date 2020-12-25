f09601f8fbc8868a73e5b8c612e7b34497d9b67d
Max Görner <max@familie-goerner.eu>
2020-12-25 14:21:34 +0100

    Check proper sudo permissions at startup
    This allows to stop the script in a clean way.

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
 DRV_NAME=rtl88x2bu
 
+if ! sudo -v
+then
+    echo "Root privileges required to run this script!" >&2
+    exit 1
+fi
+
 sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
 sudo dkms add -m ${DRV_NAME} -v ${VER}
 sudo dkms build -m ${DRV_NAME} -v ${VER}
