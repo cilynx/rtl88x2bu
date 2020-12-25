57e556e4191247bacf8c9a7ebde62154f505d943
Max Görner <max@familie-goerner.eu>
2020-12-25 14:19:36 +0100

    Add sudo again to build.sh
    At least on Debian dkms requires root privileges. Therefore, `sudo` is
    prepended.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
 DRV_NAME=rtl88x2bu
 
 sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
-dkms add -m ${DRV_NAME} -v ${VER}
-dkms build -m ${DRV_NAME} -v ${VER}
-dkms install -m ${DRV_NAME} -v ${VER}
+sudo dkms add -m ${DRV_NAME} -v ${VER}
+sudo dkms build -m ${DRV_NAME} -v ${VER}
+sudo dkms install -m ${DRV_NAME} -v ${VER}
 sudo modprobe 88x2bu
