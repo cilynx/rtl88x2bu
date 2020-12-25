b684ca2087686e996b54c87256a211fb554d42f0
Max Görner <max@familie-goerner.eu>
2020-12-25 14:48:16 +0100

    Stop to fail on `dkms add`
    The step `dkms add` is very fragile and relies on a quite clean state.
    Since at test time it seemed to be safe to just continue in case of
    a complaining `dkms add`, such failures are silently ignored now.

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ then
 fi
 
 sudo rsync -rh ./ /usr/src/"${DRV_NAME}"-"${VER}"
-sudo dkms add -m "${DRV_NAME}" -v "${VER}"
+sudo dkms add -m "${DRV_NAME}" -v "${VER}" 2>/dev/null || true  # continue in face of already existing modules
 sudo dkms build -m "${DRV_NAME}" -v "${VER}"
 sudo dkms install -m "${DRV_NAME}" -v "${VER}"
 sudo modprobe 88x2bu
