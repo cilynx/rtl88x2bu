e7ccab48e45869ba83d3b35a5c953c8c92573cce
Max Görner <max@familie-goerner.eu>
2020-12-25 14:23:26 +0100

    Quote variables to prevent word splitting

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+VER="$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)"
 DRV_NAME=rtl88x2bu
 
 if ! sudo -v
@@ -9,8 +9,8 @@ then
     exit 1
 fi
 
-sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
-sudo dkms add -m ${DRV_NAME} -v ${VER}
-sudo dkms build -m ${DRV_NAME} -v ${VER}
-sudo dkms install -m ${DRV_NAME} -v ${VER}
+sudo rsync -rvhP ./ /usr/src/rtl88x2bu-"${VER}"
+sudo dkms add -m "${DRV_NAME}" -v "${VER}"
+sudo dkms build -m "${DRV_NAME}" -v "${VER}"
+sudo dkms install -m "${DRV_NAME}" -v "${VER}"
 sudo modprobe 88x2bu
