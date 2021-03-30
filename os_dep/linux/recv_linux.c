8316868608bd0fc3de48dd7d5d7ebe4648f5a181
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-03-30 20:48:01 +0200

    Fix compilation for 5.12 by removing `if`
    As explained in commit f73fc40327c04152c792f75388d5d505aaf78964 of the
    Linux kernel repository, GRO_DROP can never be returned from the
    function napi_gro_receive. This means that the if-branch altered by this
    commit will always enter its body.
    
    Thus, the compilation can be fixed by simply calling the code first and
    calling its body afterwards.

--- os_dep/linux/recv_linux.c
+++ os_dep/linux/recv_linux.c
@@ -355,8 +355,13 @@ static int napi_recv(_adapter *padapter, int budget)
 
 #ifdef CONFIG_RTW_GRO
 		if (pregistrypriv->en_gro) {
+			#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0))
+			rtw_napi_gro_receive(&padapter->napi, pskb);
+			rx_ok = _TRUE;
+			#else
 			if (rtw_napi_gro_receive(&padapter->napi, pskb) != GRO_DROP)
 				rx_ok = _TRUE;
+			#endif
 			goto next;
 		}
 #endif /* CONFIG_RTW_GRO */
