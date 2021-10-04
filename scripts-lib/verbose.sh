77e28a1b7c7badfa94f79da6b981c1aa70b2cfd8
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-10-04 20:01:06 +0200

    Avoid unset variable error in verbose.sh

--- scripts-lib/verbose.sh
+++ scripts-lib/verbose.sh
@@ -5,16 +5,28 @@
 #     - Max Görner
 # License: GPL v3.0
 
-case "$1" in
-    --verbose)
-        echo "* verbose mode enabled"
-        # shellcheck disable=SC2034
-        V=("$1")
-        set -x
-        ;;
-    "")
-        ;;
-    *)
-        echo "usage: $0 [--verbose]"
-        exit 1
-esac
+function exit_with_errmsg() {
+    echo "usage: $0 [--verbose]" >&2
+    exit 1
+}
+
+if [[ $# -eq 0 ]]
+then
+    V=()
+elif [[ $# -eq 1 ]]
+then
+    case "${1}" in
+        --verbose)
+            echo "* verbose mode enabled"
+            # shellcheck disable=SC2034
+            V=("$1")
+            set -x
+            ;;
+        "")
+            ;;
+        *)
+            exit_with_errmsg
+    esac
+else
+    exit_with_errmsg
+fi
