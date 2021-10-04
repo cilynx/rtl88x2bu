ed7085d7fb18b8f462a5ac4b6d6ed111852970af
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-10-04 19:45:18 +0200

    Drop unnecessary redirect of STDIN

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [[ -z "${INSTALLED[*]}" ]]; then
     set -e
 
     echo "* creating dkms.conf"
-    sed 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' <hid-xpadneo/dkms.conf.in >hid-xpadneo/dkms.conf
+    sed 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' hid-xpadneo/dkms.conf.in >hid-xpadneo/dkms.conf
 
     echo "* installing module (using DKMS)"
     dkms install "${V[*]}" "hid-xpadneo" --force || cat_dkms_make_log
