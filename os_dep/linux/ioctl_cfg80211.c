4ff337e42cbaf9e9721ba319d9143a78bc7cd3b9
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-08-04 22:52:16 +0200

    Add extra unused argument link_id
    The expected function signature now contains an unsigned int for a
    link_id. Such an argument is added to please the compiler, but left
    unused otherwise.

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -5343,7 +5343,11 @@ static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *nd
 	return ret;
 }
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 0, 0))
+static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev, unsigned int link_id)
+#else
 static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev)
+#endif
 {
 	_adapter *adapter = (_adapter *)rtw_netdev_priv(ndev);
 
