2f224a9a8926e589e8e85fad0ec325f39a72f80a
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-08-10 11:12:25 +0200

    Fix one occassion of Werror=address
    With GCC 12 gcc started to detect if comparisons to addresses are always
    true. This could indicate a bug, as seen in the Linux kernel. For
    instance, a simple overlooked ampersand could turn a useful comparison
    to a tautological default, e.g. in `&my_struct->my_value`.
    
    In this particular case the function guarded by the condition is empty
    for Linux. It has implementations only for FreeBSD and Windows. This,
    the entire block is removed.

--- core/rtw_sta_mgt.c
+++ core/rtw_sta_mgt.c
@@ -380,13 +380,8 @@ static void	_rtw_free_sta_recv_priv_lock(struct sta_recv_priv *psta_recvpriv)
 void rtw_mfree_stainfo(struct sta_info *psta);
 void rtw_mfree_stainfo(struct sta_info *psta)
 {
-
-	if (&psta->lock != NULL)
-		_rtw_spinlock_free(&psta->lock);
-
 	_rtw_free_sta_xmit_priv_lock(&psta->sta_xmitpriv);
 	_rtw_free_sta_recv_priv_lock(&psta->sta_recvpriv);
-
 }
 
 
