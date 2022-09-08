365efd6875ba1d06e844af3ab659c130255b603c
Daniel Huertas <huertas.dani@gmail.com>
2022-09-08 19:27:02 +0200

    Add support for CentOS 9 Stream.

--- include/basic_types.h
+++ include/basic_types.h
@@ -80,6 +80,11 @@
 
 	typedef	signed int sint;
 
+	#ifndef RHEL_RELEASE_CODE
+	#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+	#define RHEL_RELEASE_CODE 0
+	#endif
+
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 19))
 typedef _Bool bool;
 
