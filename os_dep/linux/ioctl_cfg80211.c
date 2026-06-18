e6a98a5593f4cab2717da7cc9939b4d67cb53897
Max Görner <5477952+MaxG87@users.noreply.github.com>
2026-06-18 22:37:06 +0200

    Add rx_addr to cfg80211_rtw_remain_on_channel
    In Linux Kernel commit 4dbd1829045ee4146ae16b0e1ad122d855a694cb a new
    parameter is introduced to some functions. These require to add the
    rx_addr parameter to one helper function too.

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -7077,7 +7077,11 @@ static s32 cfg80211_rtw_remain_on_channel(struct wiphy *wiphy,
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3, 8, 0))
 	enum nl80211_channel_type channel_type,
 #endif
-	unsigned int duration, u64 *cookie)
+	unsigned int duration, u64 *cookie
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(7, 2, 0))
+    ,const u8 *rx_addr
+#endif
+)
 {
 	s32 err = 0;
 	u8 remain_ch = (u8) ieee80211_frequency_to_channel(channel->center_freq);
